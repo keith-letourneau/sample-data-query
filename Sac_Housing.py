fa8cb1727fe6a12728c9ad542ca781a9bdae6f6f
Keith <60371048+keith-letourneau@users.noreply.github.com>
2020-11-05 13:01:39 -0500

    Update Sac_Housing.py

--- Sac_Housing.py
+++ Sac_Housing.py
@@ -1,29 +1,22 @@
+#import pandas library
 import pandas as pd 
 
 #open and read spreadsheet (update file path to wherever you saved the sample dataset)
 #note the 'price' column is designated as intergers in order to correctly meet conditions
-
 sac_housing = pd.read_csv (r'C:\Users\Desktop\Sacramentorealestatetransactions.csv', dtype={'price':'int'})
 
 #set query conditions
-
 query = sac_housing.query('city.str.contains("FOLSOM|SACRAMENTO") and price > 500000', engine='python')
 query = query.sort_values(by ='city')
 
 #save results to a new/seperate spreadsheet
-
 def new_spread(): 
-
     """
     This function will transport the queried data to a new spreadsheet 
     """
-
     writer = pd.ExcelWriter('Exported from Python.xlsx')
-
     query.to_excel(writer)
-
     writer.save()
-
     print('DataFrame is written successfully to Excel File.')
 
 new_spread()
